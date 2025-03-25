4e4f890644b0966dc9a8583d5aa060e6d17a2aad
Gabrielius Daniel <daniel.gabrielius97@gmail.com>
2025-03-25 14:47:33 +0000

    Update script.js

--- Web development resources/js/script.js
+++ Web development resources/js/script.js
@@ -28,3 +28,36 @@ const headerEl = document.querySelector(".header");
 btnNavEl.addEventListener("click", function () {
   headerEl.classList.toggle("nav-open");
 });
+
+
+
+// Smooth scrolling animation //
+
+const allLinks = document.querySelectorAll("a:link");
+console.log(allLinks);
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault(e);
+    const href = link.getAttribute("href");
+
+    // scroll back to top
+
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+
+    // scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+      console.log(sectionEl);
+    }
+
+    // close mobile navigation
+    if (link.classList.contains("main-nav-link"))
+      headerEl.classList.toggle("nav-open");
+  });
+});
