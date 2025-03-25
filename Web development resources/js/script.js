fafcbc11af62757868ac5ee424227bb9f3fcbdc1
Gabrielius Daniel <daniel.gabrielius97@gmail.com>
2025-03-25 13:54:02 +0000

    Add files via upload

---
+++ Web development resources/js/script.js
@@ -0,0 +1,30 @@
+// STICKY NAVIGATION //
+const toolsSection = document.querySelector(".sticky-nav");
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    }
+
+    if (ent.isIntersecting === true) document.body.classList.remove("sticky");
+  },
+  {
+    // In the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(toolsSection);
+
+// MOBIE NAVIGATION //
+
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
